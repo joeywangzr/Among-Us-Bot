34680757c3ad0ef9002e352c1f51965de86bb088
joeywangzr <joeywangzr@gmail.com>
2020-08-22 01:23:32 -0600

    added optimizations

--- amongbot.py
+++ amongbot.py
@@ -1,6 +1,9 @@
-# Automatically make roles if they are not there
+# Automatically make roles if they are not there, also automatically add permissions (dead cannot speak in among us)
 # Ignore capitalization
 # Discord server owner setup initialization (input role names, channel names)
+# unmute self, unmute specific persno, mute specific person
+# set custom channel for each discord server
+# add embeds 
 from death import deathMessages
 from end import endMessages
 
@@ -10,12 +13,24 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 
-with open("token.txt", "r") as f:
+with open('token.txt', 'r') as f:
     lines = f.readlines()
     TOKEN = lines[0].strip()
 
 bot = commands.Bot(command_prefix='=')
 bot.remove_command('help')
+
+# Starts the bot, with a status.
+@bot.event
+async def on_ready():
+    print('Amonger is online!')
+    await bot.change_presence(status = discord.Status.online, activity=discord.Game('=help'))
+
+# Error handling.
+@bot.event
+async def on_command_error(ctx, error):
+    embed = discord.Embed(title='```ERROR```', description='Invalid argument.', colour=discord.Color.orange())
+    await ctx.send(embed=embed)
 
 @bot.command(aliases=['h','help'])
 async def _h(ctx):
@@ -26,64 +41,68 @@ async def _h(ctx):
 
 @bot.command(aliases=['gc','code','start'])
 async def _gc(ctx, code):
-    await ctx.send('The game code is ' + code)
+    embed = discord.Embed(title='GAME CODE:', description=code, colour=discord.Color.orange())
+    await ctx.send(embed=embed)
 
 @bot.command(aliases=['mute','m','ma'])
 async def _m(ctx):
-    if discord.utils.get(ctx.guild.roles, name="Head Amonger") not in ctx.author.roles:
-        await ctx.send("Only users with the Head Amonger role may use the bot.")
+    if discord.utils.get(ctx.guild.roles, name='Head Amonger') not in ctx.author.roles:
+        await ctx.send('```Only users with the Head Amonger role may use the bot.```')
         return
     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Among Us')
     for member in voiceChannel.members:
-        if discord.utils.get(ctx.guild.roles, name="Dead") not in member.roles:
+        if discord.utils.get(ctx.guild.roles, name='Dead') not in member.roles:
             await member.edit(mute=True)
         else:
-            await member.move_to(discord.utils.get(ctx.guild.voice_channels, name='Dead'))
             await member.edit(mute=False)
+            await member.move_to(discord.utils.get(ctx.guild.voice_channels, name='Dead'))
+    embed = discord.Embed(description='All users in voice channel "' + str(ctx.author.voice.channel) + '" have been muted!', colour=discord.Color.orange())
+    await ctx.send(embed=embed)
 
 @bot.command(aliases=['unmute','um','uma'])
 async def _um(ctx):
-    if discord.utils.get(ctx.guild.roles, name="Head Amonger") not in ctx.author.roles:
-        await ctx.send("Only users with the Head Amonger role may use the bot.")
+    if discord.utils.get(ctx.guild.roles, name='Head Amonger') not in ctx.author.roles:
+        await ctx.send('```Only users with the Head Amonger role may use the bot.```')
         return
     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Among Us')
-    for member in voiceChannel.members:
-        if discord.utils.get(ctx.guild.roles, name="Dead") in member.roles:
-            await member.edit(mute=True)
-        else:
-            await member.edit(mute=False)
     deadChannel = discord.utils.get(ctx.guild.voice_channels, name='Dead')
     for member in deadChannel.members:
-        await member.edit(mute=True)
+        # await member.edit(mute=True)
         await member.move_to(discord.utils.get(ctx.guild.voice_channels, name='Among Us'))
-    await ctx.send('All users in voice channel "' + str(ctx.author.voice.channel) + '" have been unmuted!')
+        await member.edit(mute=True)
+    for member in voiceChannel.members:
+        if discord.utils.get(ctx.guild.roles, name='Dead') not in member.roles:
+            await member.edit(mute=False)
+    embed = discord.Embed(description='All users in voice channel "' + str(ctx.author.voice.channel) + '" have been unmuted!', colour=discord.Color.orange())
+    await ctx.send(embed=embed)
 
 @bot.command(aliases=['dead','d'])
 async def _d(ctx, user:discord.Member):
-    if discord.utils.get(ctx.guild.roles, name="Head Amonger") not in ctx.author.roles:
-        await ctx.send("Only users with the Head Amonger role may use the bot.")
+    if discord.utils.get(ctx.guild.roles, name='Head Amonger') not in ctx.author.roles:
+        await ctx.send('```Only users with the Head Amonger role may use the bot.```')
         return
     try:# users can mute themselves if they die
-        await ctx.send(user.name + random.choice(deathMessages))
+        embed = discord.Embed(description=user.name + random.choice(deathMessages), colour=discord.Color.orange())
+        await ctx.send(embed=embed)
         await user.edit(mute=True)
-        await user.add_roles(discord.utils.get(ctx.guild.roles, name="Dead"))
+        await user.add_roles(discord.utils.get(ctx.guild.roles, name='Dead'))
     except:
-        await ctx.send('Error: user not connected to voice channel.')
+        embed = discord.Embed(description='User is not connected to a voice chanel.', colour=discord.Color.orange())
+        await ctx.send(embed=embed)
 
 @bot.command(aliases=['end','gg','e']) 
 async def _gg(ctx):
-    await ctx.send(random.choice(endMessages))
+    embed = discord.Embed(description=random.choice(endMessages), colour=discord.Color.orange())
+    await ctx.send(embed=embed)
     deadChannel = discord.utils.get(ctx.guild.voice_channels, name='Dead')
     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Among Us')
     for member in deadChannel.members:
         await member.move_to(voiceChannel)
+    for member in voiceChannel.members:
+        await member.edit(mute=False)
     for member in ctx.guild.members:
-        if discord.utils.get(ctx.guild.roles, name="Dead") in member.roles:
-            await member.remove_roles(discord.utils.get(ctx.guild.roles, name="Dead"))
-        try:
-            await member.edit(mute=False)
-        except:
-            continue
+        if discord.utils.get(ctx.guild.roles, name='Dead') in member.roles:
+            await member.remove_roles(discord.utils.get(ctx.guild.roles, name='Dead'))
 
 bot.run(TOKEN)
 
